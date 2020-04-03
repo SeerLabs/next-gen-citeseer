48f0c709cac4ee68d2aeea5050e9f0c49898c110
Kevin Kuo <kevinkuo15@gmail.com>
2020-04-03 00:54:39 -0400

    doc view page initial commit

---
+++ client/pages/doc_view/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <navbar/>
+    <search-box/>
+  </div>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar.vue'
+import SearchBox from '~/components/SearchBox.vue'
+export default {
+    components: {
+        Navbar,
+        SearchBox
+    }
+}
+</script>
+
+<style>
+
+</style>
