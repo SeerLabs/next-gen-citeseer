c373d900720309a61424db34d698157bb3d51833
Raghav Keesara <raghavkeesara@gmail.com>
2020-06-12 02:58:56 -0400

    Integerate Citations Backend API (#9)

--- server/app/models/api_response.py
+++ server/app/models/api_response.py
@@ -2,6 +2,7 @@ from typing import List
 
 from pydantic import BaseModel
 
+from models.citation import Citation
 from models.paper import Paper
 
 
@@ -13,3 +14,9 @@ class SearchQueryResponse(BaseModel):
 class PaperDetailResponse(BaseModel):
     query_id: str
     paper: Paper
+
+class CitationsResponse(BaseModel):
+    query_id: str
+    total_results: int
+    citations: List[Citation]
+
