fcce06ab1597956ecd42f1525c28c79658d0d60c
sai_raghav <raghavkeesara@gmail.com>
2020-07-05 17:43:59 -0400

    cleanup backend + add sort func for showCiting

--- server/app/routers/elastic_routes.py
+++ server/app/routers/elastic_routes.py
@@ -9,15 +9,15 @@ from models.citation import Citation
 from models.cluster import Cluster
 from models.paper import Paper
 from models.search_query import SearchQuery
-from services.elastic_service import Elastic
+from services.elastic_service import ElasticService
 
 router = APIRouter()
-ES = Elastic()
+elastic_service = ElasticService()
 
 
 @router.post('/search', response_model=SearchQueryResponse)
 def perform_search(searchQuery: SearchQuery):
-    docs_response = ES.paginated_search('citeseerx', searchQuery.queryString, searchQuery.page, searchQuery.pageSize,
+    docs_response = elastic_service.paginated_search('citeseerx', searchQuery.queryString, searchQuery.page, searchQuery.pageSize,
                                         ['title', 'text'])
     result_list = []
     for doc_hit in docs_response['hits']['hits']:
@@ -28,14 +28,14 @@ def perform_search(searchQuery: SearchQuery):
 
 @router.get('/paper/{id}')
 def paper_info(id: str):
-    docs_response = ES.get_paper_info(id)
+    docs_response = elastic_service.get_paper_info(id)
     paper_entity_response = build_paper_entity(docs_response['hits']['hits'][0]['_source'])
     return PaperDetailResponse(query_id=str(uuid4()), paper=paper_entity_response)
 
 
 @router.get('/citations/{id}')
 def citations(id: str, page: int = 1, pageSize: int = 10):
-    es_citations_response = ES.paginated_search('citations', id, page, pageSize, 'paperid')
+    es_citations_response = elastic_service.paginated_search('citations', id, page, pageSize, 'paperid')
     result_list = []
     for doc_hit in es_citations_response['hits']['hits']:
         result_list.append(build_citation_entity(doc=doc_hit['_source']))
@@ -45,25 +45,25 @@ def citations(id: str, page: int = 1, pageSize: int = 10):
 
 @router.get('/cluster/{cid}')
 def show_cluster_detail(cid: str):
-    cluster_detail_response = ES.get_single_cluster_details(cid)
+    cluster_detail_response = elastic_service.get_cluster_info(cid)
     return ClusterDetailResponse(query_id=str(uuid4()),
                                  cluster=build_cluster_entity(cluster_detail_response['_source']))
 
 
 @router.get('/showCiting/{cid}')
 def show_citing(cid: str, sort: str, page: int, pageSize: int):
-    cluster_response = ES.get_single_cluster_details(cid)
+    cluster_response = elastic_service.get_cluster_info(cid)
     print(cluster_response)
     primary_cluster_detail = build_cluster_entity(cluster_response['_source'])
     cid_list = cluster_response['_source']['cited_by']
-    response = ES.get_paper_ids_for_clusters(cid_list=cid_list)
+    response = elastic_service.get_paper_ids_for_clusters(cid_list=cid_list)
     print(response)
     papers_list = []
     for each_hit in response['docs']:
         if 'included_papers' in each_hit['_source'] and len(each_hit['_source']['included_papers']) != 0:
             papers_list.append(each_hit['_source']['included_papers'][0])
     # print(papers_list)
-    papers_response = ES.get_sorted_papers(papers_list=papers_list, page=page, pageSize=pageSize, sort=sort)
+    papers_response = elastic_service.get_sorted_papers(papers_list=papers_list, page=page, pageSize=pageSize, sort=sort)
     print(papers_response)
     result_list = []
     for each_paper_hit in papers_response['hits']['hits']:
@@ -75,7 +75,7 @@ def show_citing(cid: str, sort: str, page: int, pageSize: int):
 
 @router.get('/similar')
 def similar_papers(paperID: str = ""):
-    result = ES.get_clustered_papers(paperID)
+    result = elastic_service.get_clustered_papers(paperID)
     return result
 
 
--- server/app/services/elastic_service.py
+++ server/app/services/elastic_service.py
@@ -2,7 +2,8 @@ from elasticsearch import Elasticsearch
 import requests
 import json
 
-class Elastic:
+
+class ElasticService:
 
     def __init__(self):
         self.connection = Elasticsearch([{'host': '130.203.139.151', 'port': 9200}])
@@ -16,13 +17,6 @@ class Elastic:
     def print_response(self, response):
         print(json.dumps(response, indent=4, sort_keys=True))
 
-    '''
-    input: index (str), the index that needs to be queried, usually citeseerx for papers or authors for authors
-            q (str), the string query that is being made to Elastic
-            source (list), the list of strings that represent the fields that must be included in the ES response
-            fields_to_search (list), the list of strings that are going to searched for the query
-    
-    '''
     def paginated_search(self, index, query, page, pageSize, fields_to_search, source=None):
 
         body = {
@@ -63,38 +57,26 @@ class Elastic:
         #Search the citeseerx index for this specific paperID and return all fields associated with a hit
         return self.paginated_search('citeseerx', paperID, 1, 10, 'paper_id')
 
-    # Given a paperID, return a few similar or clustered documents from the cluster index
-    # There is a more simple way of thinking of this function, we could just use clusterID if we have it cached either in front end or backend for specific paper
-    def get_clustered_papers(self, paperID):
+    def get_clustered_papers(self, paper_id):
+        """Given a paperID, return a few similar or clustered documents from the cluster index"""
         # Search the cluster index for a cluster which contains this specific paperID, return all other included papers
-        return self.paginated_search('clusters', paperID, 1, 10, 'included_papers', ['included_papers'])
+        return self.paginated_search('clusters', paper_id, 1, 10, 'included_papers', ['included_papers'])
 
-    def get_single_cluster_details(self, cluster_id):
-        return self.connection.get(index="clusters2", doc_type="cluster", id = cluster_id)
+    def get_cluster_info(self, cluster_id):
+        return self.connection.get(index="clusters2", doc_type="cluster", id=cluster_id)
 
     def get_paper_ids_for_clusters(self, cid_list):
         return self.connection.mget(index="clusters2", doc_type="cluster", body={'ids': cid_list},
                                     _source_includes='included_papers')
 
     def get_sorted_papers(self, papers_list, page, pageSize, sort):
-        if sort is "yearAsc":
+        if sort == "yearAsc":
+            sort = 'year:asc'
+        elif sort == "yearDsc":
             sort = 'year:desc'
-        elif sort is "yearDsc":
-            sort = 'year:desc'
-        elif sort is "citCount":
+        elif sort == "citCount":
             sort = 'ncites:desc'
         else:
             sort = 'ncites:desc'
-        return self.paginated_search_with_ids(index="citeseerx", ids=papers_list, page=page, pageSize=pageSize,
-                                              sort=sort)
-'''
-ES = Elastic()
-ES.test_connection()
-#ES.refined_search('citeseerx', 'citeseer', ['title', 'text'])
-paperID = "10.1.1.170.7725"
-
-ES.get_paper_info(paperID)
-ES.get_citations(paperID)
-ES.get_clustered_papers(paperID)
-
-'''
+        return self.paginated_search_with_ids(index="citeseerx", ids=papers_list,
+                                              page=page, pageSize=pageSize, sort=sort)
