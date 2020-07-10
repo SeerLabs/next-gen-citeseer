65e9e67e638291042cf4378fa39c08a8c88c4877
sai_raghav <raghavkeesara@gmail.com>
2020-07-09 23:27:42 -0400

    Refactor Backend

--- server/app/routers/document_routes.py
+++ server/app/routers/document_routes.py
@@ -5,13 +5,12 @@ from starlette.responses import FileResponse
 from services.repository_service import RepositoryService
 
 router = APIRouter()
-
+rep_svc = RepositoryService()
 
 @router.get('/document')
 def get_document(key: str = "c1t3s33r", type: str = "pdf", repid: str = "rep1", doi: str = ""):
     # TODO: Implement a logging framework
     print("received a request for ", doi, " ", type, " ", repid, " ", key)
-    rep_svc = RepositoryService()
     response_content = rep_svc.get_document(doi=doi, file_type=type, repid=repid).content
     with tempfile.NamedTemporaryFile(mode="w+b", suffix=".pdf", delete=False) as FOUT:
         FOUT.write(response_content)
