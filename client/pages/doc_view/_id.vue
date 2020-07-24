e1c1292c422e0de4f6a601ad94d6708a869d4972
jasonchhay <jasonchhay@gmail.com>
2020-07-23 23:43:18 -0400

    Added smooth scrolling to table of contents, fixed implementation, fixed grid layout

--- client/pages/doc_view/_id.vue
+++ client/pages/doc_view/_id.vue
@@ -1,213 +1,252 @@
 <template>
-    <!-- $ sudo sysctl fs.inotify.max_user_watches=524288
-    $ sudo sysctl -p -->
+  <!-- $ sudo sysctl fs.inotify.max_user_watches=524288
+    $ sudo sysctl -p-->
 
+  <div id="doc-view-layout">
+    <b-container fluid>
+      <!-- Search Box Row -->
+      <b-row align-h="center">
+        <b-col cols="12">
+          <search-box />
+        </b-col>
+      </b-row>
 
+      <!-- Main Info Row -->
+      <b-row id="abstract" align-h="center">
+        <b-col cols="9">
+          <h1>{{ title }}</h1>
+          <h5>{{ authors.join(", ") }}</h5>
+          <h5>{{ venue }} - {{ year }}</h5>
+          <br />
+          <p v-if="!readMoreToggle">
+            {{ abstract.slice(0, 700) }}
+            <a href>Read more...</a>
+          </p>
+          <p v-else>
+            {{ abstract }}
+            <!-- <span v-show="readMoreToggle" v-html="abstract"></span> -->
+          </p>
+        </b-col>
+        <b-col cols="3">
+          <b-card>
+            <b-card-text>
+              <!-- PDF Button -->
+              <b-button
+                squared
+                id="pdf-btn"
+                v-bind:to="getPDFUrl"
+                target="_blank"
+                >View PDF</b-button
+              >
 
+              <!-- Download Links Drop Down -->
+              <b-dropdown
+                id="download-links-dropdown"
+                text="Download Links"
+                variant="outline-secondary"
+                class="my-md-2"
+                size="sm"
+              >
+                <b-dropdown-item>Link 1</b-dropdown-item>
+                <b-dropdown-item>Link 2</b-dropdown-item>
+                <b-dropdown-item>Link 3</b-dropdown-item>
+              </b-dropdown>
+              <ul>
+                <h6>Cite This</h6>
+                <h6>Save</h6>
+                <h6>Add to Collection</h6>
+                <h6>Add to MetaCart</h6>
+                <h6>Correct Errors</h6>
+              </ul>
+            </b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
 
-    <div id="doc-view-layout">
-        <test-child/>
-        <b-card id="toc">
-            <b-row>
-                <b-col><h6 v-on:click="scroll('citation-card')" >Citation</h6></b-col>
-            </b-row>
-            <br>
-            <b-row>
-                <b-col><h6 v-on:click="scroll('similar-article-card')" >Similar Article</h6></b-col>
-            </b-row>
-            <br>
-            <b-row>
-                <b-col><h6 v-on:click="scroll('ver-history-card')" >Version History</h6></b-col>
-            </b-row>
-            
-        </b-card>
-        <b-container fluid>
-            <!-- Search Box Row -->
-            <b-row align-h="center">
-                <b-col  cols="12"> 
-                    <search-box/>
-                </b-col>
-            </b-row>
-            <!-- Main Info Row -->
-            <b-row id="abstract" align-h="center">
-                <b-col cols="9">
-                    <h1>{{ title }}</h1>
-                    <h5>{{ authors.join(', ')}}</h5>
-                    <h5>{{ venue }} - {{ year }}</h5>
-                    <br>
-                    <p v-if="!readMoreToggle">
-                        {{abstract.slice(0, 700)}} 
-                        <a href="">
-                            Read more...
-                        </a>
-                    <p v-else>
-                        {{ abstract }}
-                        <!-- <span v-show="readMoreToggle" v-html="abstract"></span> -->
-                    </p>
-                </b-col>
-                <b-col cols="3">
-                    <b-card>
-                        <!-- PDF Button -->
-                        <b-row>
-                            <b-col><b-button squared id="pdf-btn" v-bind:to="getPDFUrl" target="_blank">View PDF</b-button></b-col>
+      <!-- Citations Row -->
+      <b-row>
+        <b-col cols="10">
+          <citation-card
+            class="citation-card"
+            id="citations"
+            title="Citations"
+            v-bind:doi="doi"
+            v-bind:ncitation="nCitation"
+          />
 
-                        </b-row>
-                        
-                        <!-- Download Links Drop Down -->
-                        <b-row>
-                            <b-dropdown id="download-links-dropdown" text="Download Links" variant="outline-secondary" class="m-md-2" size="sm">
-                                <b-dropdown-item>Link 1</b-dropdown-item>
-                                <b-dropdown-item>Link 2</b-dropdown-item>
-                                <b-dropdown-item>Link 3</b-dropdown-item>
-                            </b-dropdown>
-                        </b-row>
-                        <b-row >
-                            <b-col> <h6>Cite This</h6> </b-col>
-                        </b-row>
-                        <b-row>
-                            <b-col><h6>Save</h6></b-col>
-                        </b-row>
-                        <b-row>
-                            <b-col><h6>Add to Collection</h6></b-col>
-                        </b-row>
-                        <b-row>
-                            <b-col><h6>Add to MetaCart</h6></b-col>
-                        </b-row>
-                        <b-row>
-                            <b-col><h6>Correct Errors</h6></b-col>
-                        </b-row>
-                       
-                        
-                        
-                        
-                        
-                    </b-card>
-                </b-col>
-            </b-row>
-
-            <!-- Citations Row -->
-            <b-row id ="citation-card" class="citation-card" align-h="center">
-                <b-col cols="12">
-                    <citation-card id="citation-card" title="Citations" v-bind:doi="doi" v-bind:ncitation="nCitation"/>
-                </b-col>
-            </b-row>
-            
-            <!-- Similar Articles Row -->
-            <b-row id ="similar-article-card" class="citation-card" align-h="center">
-                <b-col cols="12">
-                    <citation-card id="citation-card" title="Similar Articles" v-bind:doi="doi" citation=""/>
-                </b-col>
-            </b-row>
-
-            <!-- Similar Articles Row -->
-            <b-row id="ver-history-card" class="citation-card" align-h="center">
-                <b-col cols="12">
-                    <version-history-card title="Version History"/>
-                </b-col>
-            </b-row>
-        </b-container>    
-    </div>
+          <citation-card
+            class="citation-card"
+            id="similar-articles"
+            title="Similar Articles"
+            v-bind:doi="doi"
+            citation
+          />
+          <version-history-card id="version-history" title="Version History" />
+        </b-col>
+        <b-col cols="2">
+          <b-card id="table-of-contents">
+            <a href="#citations">
+              <h6>Citation</h6>
+            </a>
+            <a
+              href="#similar-articles"
+              v-on:click="scroll('similar-article-card')"
+            >
+              <h6>Similar Articles</h6>
+            </a>
+            <a href="#version-history">
+              <h6>Version History</h6>
+            </a>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
-    import Navbar from '~/components/Navbar.vue'
-    import TableOfContent from '~/components/DocumentView/TableOfContent.vue'
-    import SearchBox from '~/components/SearchBox.vue'
-    import CitationCard from '~/components/DocumentView/CitationCard.vue'
-    import BaseCard from '~/components/Base/BaseCard.vue'
-    import DocViewService from "~/api/DocViewService"
-    import DocumentResultsList from "~/components/DocumentResults/DocumentResultsList.vue"
-    import VersionHistoryCard from "~/components/DocumentView/VersionHistoryCard.vue"
-    export default {
-        components: {
-            Navbar,
-            TableOfContent,
-            SearchBox,
-            CitationCard,
-            BaseCard,
-            DocumentResultsList,
-            VersionHistoryCard,
-        },
-        methods: {
-            toggleReadMore : function(){
-                this.readMoreFlag = true
-            },
-            scroll(id) {
-              document.getElementById(id).scrollIntoView();
-            }
-        },
-        data (){
-            return {
-                readMoreToggle: false,
-                title : "", 
-                year : "",
-                authors : [],
-                venue : "",
-                abstract : "",
-                nCitation : "",
+import Navbar from "~/components/Navbar.vue";
+import TableOfContent from "~/components/DocumentView/TableOfContent.vue";
+import SearchBox from "~/components/SearchBox.vue";
+import CitationCard from "~/components/DocumentView/CitationCard.vue";
+import BaseCard from "~/components/Base/BaseCard.vue";
+import DocViewService from "~/api/DocViewService";
+import DocumentResultsList from "~/components/DocumentResults/DocumentResultsList.vue";
+import VersionHistoryCard from "~/components/DocumentView/VersionHistoryCard.vue";
 
-                 documents: [
-              {'title': 'Document Title', 'type': 'DOCUMENT', 'authors': 'Abcdefg Lastname',
-              'year': '2018', 'abstract': 'Lorem ipsum', 'numCitations': 20},
-              {'title': 'ABCDEFG', 'type': 'DOCUMENT', 'authors': 'Hijklmno Pqrstuv',
-              'year': '2020', 'abstract': 'Lorem ipsum', 'numCitations': 3},
-              {'title': 'EFGHIJK', 'type': 'CITATION', 'authors': 'Firstname Lastname',
-              'year': '2021', 'abstract': 'Lorem ipsum', 'numCitations': 30}
-            ],
-            totalPageResults: 1000
-            }
-        },
-        async fetch() {
-            console.log(this.$route.params.id);
-            const { data } = await DocViewService.getPaperEntity(this.$route.params.id)
-            console.log(data);
+import $ from "jquery";
 
-            this.title = data.paper.title, 
-            this.year = data.paper.year,
-            this.authors = data.paper.authors,
-            this.venue = data.paper.venue,
-            this.abstract = data.paper.abstract,
-            this.nCitation = data.paper.total_results
-        },
-        computed: {
-            getPDFUrl() {
-                return '/pdf/' + this.$route.params.id;
-            }
-        },
-        layout: 'layout_default',
-        
-        
+export default {
+  components: {
+    Navbar,
+    TableOfContent,
+    SearchBox,
+    CitationCard,
+    BaseCard,
+    DocumentResultsList,
+    VersionHistoryCard
+  },
+  methods: {
+    toggleReadMore: function() {
+      this.readMoreFlag = true;
+    },
+    scroll: function(id) {
+      return null;
+      // document.getElementById(id).scrollIntoView();
     }
+  },
+  data() {
+    return {
+      readMoreToggle: false,
+      title: "",
+      year: "",
+      authors: [],
+      venue: "",
+      abstract: "",
+      nCitation: "",
+
+      documents: [
+        {
+          title: "Document Title",
+          type: "DOCUMENT",
+          authors: "Abcdefg Lastname",
+          year: "2018",
+          abstract: "Lorem ipsum",
+          numCitations: 20
+        },
+        {
+          title: "ABCDEFG",
+          type: "DOCUMENT",
+          authors: "Hijklmno Pqrstuv",
+          year: "2020",
+          abstract: "Lorem ipsum",
+          numCitations: 3
+        },
+        {
+          title: "EFGHIJK",
+          type: "CITATION",
+          authors: "Firstname Lastname",
+          year: "2021",
+          abstract: "Lorem ipsum",
+          numCitations: 30
+        }
+      ],
+      totalPageResults: 1000
+    };
+  },
+  mounted: function() {
+    $("#table-of-contents a").on("click", function(e) {
+      console.log("Press");
+      e.preventDefault();
+
+      var hash = this.hash;
+      console.log(hash);
+
+      // animate
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        300,
+        function() {
+          window.location.hash = hash;
+        }
+      );
+    });
+  },
+  async fetch() {
+    console.log(this.$route.params.id);
+    const { data } = await DocViewService.getPaperEntity(this.$route.params.id);
+    console.log(data);
+
+    (this.title = data.paper.title),
+      (this.year = data.paper.year),
+      (this.authors = data.paper.authors),
+      (this.venue = data.paper.venue),
+      (this.abstract = data.paper.abstract),
+      (this.nCitation = data.paper.total_results);
+  },
+  computed: {
+    getPDFUrl() {
+      return "/pdf/" + this.$route.params.id;
+    }
+  },
+  layout: "layout_default"
+};
 </script>
 
 <style>
-#doc-view-layout{
-    background: rgb(255, 255, 255);
+#doc-view-layout {
+  background: rgb(255, 255, 255);
 }
-#side-margine {
 
-}
 #abstract {
-    margin-top: 4%;
-    margin-bottom: 2%;
-    background: #ffffff;
+  margin-top: 4%;
+  margin-bottom: 2%;
+  background: #ffffff;
 }
+
 #summary-text {
-    padding-top: 2%;
-    padding-bottom: 2%;
+  padding-top: 2%;
+  padding-bottom: 2%;
 }
+
 .citation-card {
-    margin-top: 3%;
-    margin-bottom: 3%;
+  margin-bottom: 1em;
 }
+
 #pdf-btn {
-    background: rgb(235, 0, 0);
-    outline: transparent;
-    border-color: transparent;
+  background: rgb(235, 0, 0);
+  outline: transparent;
+  border-color: transparent;
 }
-#toc {
-    position: fixed;
-    right: 5%;
-    margin-top: 20%;
+
+#table-of-contents {
+  position: sticky;
+  top: 5em;
+}
+
+#table-of-contents .card-body {
+  padding: 1rem;
 }
 </style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,27 +1,27 @@
 <template>
     <div id="app">
-      <h1>Search Engine</h1>
-      <search-box/>
+        <h1>Search Engine</h1>
+        <search-box />
     </div>
 </template>
 
 <script>
-import SearchBox from '~/components/SearchBox.vue'
+import SearchBox from "~/components/SearchBox.vue";
 export default {
-  components: {
-    SearchBox
-  },
-  layout: 'layout_default',
+    components: {
+        SearchBox,
+    },
+    layout: "layout_default",
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin: 10% auto;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin: 10% auto;
 }
 </style>
