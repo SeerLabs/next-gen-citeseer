255276365f7f103dd3db7b2e6ffc8106160be338
sai_raghav <raghavkeesara@gmail.com>
2020-08-12 19:35:15 -0400

    Enhancing the utility function to include a default value

--- server/app/utils/helpers.py
+++ server/app/utils/helpers.py
@@ -1,2 +1,4 @@
-def getKeyOrNone(attrdict, key):
-  return attrdict[key] if key in attrdict else None
+def getKeyOrDefault(attrdict, key, default=None):
+    if attrdict is None:
+        return default
+    return attrdict[key] if key in attrdict else default
