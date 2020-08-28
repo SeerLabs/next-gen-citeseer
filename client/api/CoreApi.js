dfa314f9f84af4e8361d538501a06d4dd1214905
jasonchhay <jasonchhay@gmail.com>
2020-08-28 14:50:45 -0400

    Reverted base API url

--- client/api/CoreApi.js
+++ client/api/CoreApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default () => {
     return axios.create({
-        baseURL: 'http://127.0.0.1:8002/api/',
+        baseURL: 'http://localhost:8000/api/',
         withCredentials: false,
         headers: {
             Accept: 'application/json',
