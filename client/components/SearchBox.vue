e99daaebfe63d1511e694716bdd4fd70e74704c3
jasonchhay <jasonchhay@gmail.com>
2020-08-28 01:30:18 -0400

    Updated search box to combo box, now allows for custom input, fixed with URL queries and suggestions not showing up properly

--- client/components/SearchBox.vue
+++ client/components/SearchBox.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-autocomplete
+    <v-combobox
         v-model="searchQuery"
         :items="items"
         :loading="isLoading"
@@ -8,17 +8,17 @@
         clearable
         hide-no-data
         hide-selected
-        item-text="Description"
-        item-value="API"
+        item-text="description"
+        item-value="title"
         placeholder="Search"
-        @keyup.enter="submitInput"
+        @keydown.enter="submitInput"
     >
         <template v-slot:append>
             <div id="search-button" @click="submitInput">
                 <v-icon class="ml-3">search</v-icon>
             </div>
         </template>
-    </v-autocomplete>
+    </v-combobox>
 </template>
 
 <script>
@@ -44,17 +44,18 @@ export default {
                         ? entry.slice(0, this.descriptionLimit) + '...'
                         : entry;
 
-                return Object.assign({}, entry, { Description });
+                return { title: entry, description: Description };
             });
         }
     },
     watch: {
         textInput(val) {
-            if (val == null) {
-                return;
-            }
+            // Items have already been requested
+            if (this.textInput === this.searchQuery) return;
+
+            if (this.isLoading) return;
+
             this.isLoading = true;
-            console.log(this.textInput);
 
             searchPaperService
                 .getSuggestions(this.textInput)
@@ -63,28 +64,33 @@ export default {
                     this.entries = response.data.suggestions.map(
                         (suggestion) => suggestion.text
                     );
-                    console.log(this.entries);
                 })
                 .catch((error) => {
                     // eslint-disable-next-line
                     console.log(error.message);
                     this.error = true;
                 })
-                .finally(() => {
-                    this.isLoading = false;
-                });
+                .finally(() => (this.isLoading = false));
+        },
+        searchQuery() {
+            // Note, item-value is currently not working on v-combobox as a known issue with Vuetify
+            // As a workaround, we check to make sure that the searchQuery exists before pulling its value
+            this.textInput = this.searchQuery ? this.searchQuery.title : '';
+            this.submitInput();
         }
     },
     created() {
-        console.log(this.$route.query.query);
         this.textInput = this.$route.query.query || '';
+        this.searchQuery = this.textInput;
     },
     methods: {
         submitInput() {
-            if (this.searchQuery) {
+            if (this.textInput) {
+                this.entries = [];
+
                 this.$router.push({
                     name: 'search_result',
-                    query: { query: this.searchQuery }
+                    query: { query: this.textInput }
                 });
             }
         }
