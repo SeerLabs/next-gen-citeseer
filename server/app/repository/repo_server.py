536f974070a7d04d6e2e4fdd74bd15f646dfc0ad
Sai Raghav <raghavkeesara@gmail.com>
2021-01-04 17:06:40 -0500

    fix repo server

--- server/app/repository/repo_server.py
+++ server/app/repository/repo_server.py
@@ -1,11 +1,13 @@
 from typing import Optional
 
 import uvicorn as uvicorn
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import FileResponse
 
 app = FastAPI()
+
 
 # origins = [
 #     "http://localhost:3000",
@@ -20,7 +22,12 @@ app = FastAPI()
 
 @app.get("/document")
 def get_pdf(doi: str, type: Optional[str] = "pdf", rep_id: Optional[str] = "1"):
-    return FileResponse("/data/repository/"+ doi[:2] + "/"+doi+"."+type)
+    chunks = [doi[i:i + 2] for i in range(0, len(doi), 2)]
+    filename = doi + "." + type
+    pdf_repo_path = os.path.join("/data/repo/", chunks[0],
+                                 chunks[1], chunks[2], chunks[3], chunks[4], chunks[5],
+                                 chunks[6], doi, filename)
+    return FileResponse(pdf_repo_path)
 
 
 if __name__ == "__main__":
