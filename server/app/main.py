60080b28031595050a61c24aefaba4934cda329b
Jason Chhay <jasonchhay@gmail.com>
2021-01-07 13:57:53 -0500

    Added host to allow origins

--- server/app/main.py
+++ server/app/main.py
@@ -9,11 +9,12 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://localhost:8081"
+    "http://localhost:8000",
     "http://0.0.0.0:3000/"
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
