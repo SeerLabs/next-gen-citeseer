68342697690daf4f19b20b1b20821160b02943a1
Jason Chhay <jasonchhay@gmail.com>
2021-01-07 15:19:25 -0500

    Fixed environmental variables not loading

--- client/api/CoreApi.js
+++ client/api/CoreApi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 var API_URL
 
-if (process.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // SWAP OUT WITH PUBLIC API
     API_URL = 'http://istcsxfe01.ist.psu.edu:8080/api'
 }
