51747b62b237f10f9d664bb733c23b7a8267e2b8
jasonchhay <jason@ISTCSXFE01.ist.psu.edu>
2021-01-07 16:06:58 -0500

    Fixed production API

--- client/api/CoreApi.js
+++ client/api/CoreApi.js
@@ -4,7 +4,7 @@ var API_URL
 
 if (process.env.NODE_ENV === 'production') {
     // SWAP OUT WITH PUBLIC API
-    API_URL = 'http://istcsxfe01.ist.psu.edu:8080/api'
+    API_URL = 'http://localhost:8080/api'
 }
 else {
     API_URL = 'http://localhost:8000/api'
