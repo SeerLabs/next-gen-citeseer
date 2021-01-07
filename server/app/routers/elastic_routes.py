f696c3e010958d3b4c3a6c97b2e1166c3c008163
Jason Chhay <jasonchhay@gmail.com>
2021-01-07 16:50:46 -0500

    Updateed to use ES cluster

--- server/app/routers/elastic_routes.py
+++ server/app/routers/elastic_routes.py
@@ -34,8 +34,16 @@ def perform_search(searchQuery: SearchQuery):
 
 @router.post('/search/aggregations', response_model=AggregationsResponse)
 def get_aggregations_from_query(aggsQuery: AggregationsQuery):
-    s = elastic_service.get_aggregations('citeseerx', aggsQuery.queryString, ['title', 'text'])
-
+    aggs_fields = [{'key': 'authors', 'field_name': 'authors.name.keyword'}]
+    s = elastic_models.Cluster.search(using=elastic_service.get_connection())
+    
+    s.query = Q('multi_match', query=aggsQuery.queryString, fields=['title', 'text'])
+    for item in aggs_fields:
+      a = A('terms', field=item['field_name'], size=10, order={'_count': 'desc'})
+      s.aggs.bucket(item['key'], a)
+    s = s[0:0]
+    response = s.execute()['aggregations']
+    
     aggregations = []
     for key in response:
       aggs_list = []
