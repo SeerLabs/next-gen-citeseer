d6eb5e3396136cb5ec542900279bc97d94f8f1a1
Jason Chhay <jasonchhay@gmail.com>
2021-04-23 14:25:22 -0400

    Updated PDF route

--- client/pages/pdf/_id.vue
+++ client/pages/pdf/_id.vue
@@ -10,11 +10,11 @@
 export default {
   data () {
     return {
-      pdfURL: 'http://localhost:8000/api/document?repid=rep1&type=pdf&doi='
+      pdfURL: `http://localhost:8889/document?repid=rep1&type=pdf&doi=`
     }
   },
   mounted () {
-    this.pdfURL += this.$route.params.id
+    this.pdfURL = this.$route.params.id
   }
 }
 </script>
