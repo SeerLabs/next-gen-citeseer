4c0ab1d6d47fd91c0ab51b30d6c9a0b97f0f44c2
Jason Chhay <jasonchhay@gmail.com>
2021-05-04 10:00:27 -0400

    Updated call for axios on PDF page

--- client/pages/pdf/_id.vue
+++ client/pages/pdf/_id.vue
@@ -10,7 +10,7 @@
 export default {
   data () {
     return {
-      pdfURL: `${$axios.defaults.baseURL}/document?repid=rep1&type=pdf&doi=`
+      pdfURL: `${this.$axios.defaults.baseURL}/document?repid=rep1&type=pdf&doi=`
     }
   },
   mounted () {
