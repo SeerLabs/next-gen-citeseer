48274e37a4031a59519108284fd9dfe004dc545b
Kavya Shrinivas Puranik <kzp5555@i4-lx-csxfe02.ad.psu.edu>
2022-07-30 13:58:35 -0400

    index

--- server/app/settings.py
+++ server/app/settings.py
@@ -1,6 +1,6 @@
 #CLUSTERS_INDEX = "acl_papers_faceted_latest"
 #CLUSTERS_INDEX = "csx_citeseer_all_docs"
-CLUSTERS_INDEX = "csx_citeseer_papers"
+CLUSTERS_INDEX = "csx_citeseer_docs"
 #KEYMAP_INDEX = "key_map04"
 KEYMAP_INDEX = "csx_citeseer_papers_keymap"
 REQUESTS_INDEX = "requestsdemo"
