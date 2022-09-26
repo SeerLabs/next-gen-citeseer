c9d72adcc4e21e35c4776657c851db4aec9e1435
Kavya Shrinivas Puranik <kzp5555@i4-lx-csxfe02.ad.psu.edu>
2022-09-26 14:23:42 -0400

    repo path and index

--- server/app/settings.py
+++ server/app/settings.py
@@ -1,10 +1,11 @@
 #CLUSTERS_INDEX = "acl_papers_faceted_latest"
 #CLUSTERS_INDEX = "csx_citeseer_all_docs"
-CLUSTERS_INDEX = "csx_citeseer_docs"
+#CLUSTERS_INDEX = "csx_citeseer_docs"
+CLUSTERS_INDEX = "csx_citeseer_docs_old"
 #KEYMAP_INDEX = "key_map04"
 KEYMAP_INDEX = "csx_citeseer_papers_keymap"
 REQUESTS_INDEX = "requestsdemo"
 #REPO_SERVER_BASE_PATH = "/mnt/rep1/"
-REPO_SERVER_BASE_PATH = "/data/csxrepo01_repo/"
+REPO_SERVER_BASE_PATH = "/data/csxrepo01_repo/csx_beta_repo/"
 REPO_SERVER_HOST = "localhost"
 
