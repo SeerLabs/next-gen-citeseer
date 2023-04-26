78c9bc660a3e0d5478b11038924fd6b0436733c7
Kavya Shrinivas Puranik <kzp5555@i4-lx-csxstaging.ad.psu.edu>
2023-04-26 11:46:47 -0400

    removed unnecessary files

--- server/app/main.py
+++ server/app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.responses import JSONResponse, PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
 import os
-from routers import document_routes, elastic_routes, authentication_routes, redirect_routes, search_routes
+from routers import document_routes, elastic_routes, authentication_routes, redirect_routes
 from limiter import limiter
 import requests
 
@@ -48,7 +48,6 @@ app.include_router(
     authentication_routes.router, tags=["authentication_routes"], prefix="/api"
 )
 app.include_router(redirect_routes.router, tags=["redirect_routes"], prefix="/viewdoc")
-app.include_router(redirect_routes.router, tags=["search_routes"], prefix="/search")
 
 @app.exception_handler(AuthJWTException)
 def authjwt_exception_handler(request: Request, exc: AuthJWTException):
